d25e2839cd4127dc8ae01beaa20e5b14c82d134d
Tomotada Saigusa <nirot1r@g-second.net>
2018-09-08 01:27:44 +0900

    #17 他チャンネルへの通知

--- modules/eq.js
+++ modules/eq.js
@@ -2,10 +2,8 @@ require('date-utils');
 
 const app = require('../app');
 const { km, nhkeq } = require('../lib/eq');
+const discord = require('../modules/discord');
 const discord_config = require('../config/discord');
-
-let eq_list = { };
-let nhkeq_list = [];
 
 exports.km = () => {
   const ei = km();
@@ -16,42 +14,50 @@ exports.km = () => {
       return;
     } // 起動時の時間より前の発表を無視する(#2)
 
-    if(!(ei.report_id in eq_list)) {
+    if(!(ei.report_id in app.eq_list)) {
       console.log(`modules.eq[km] : new eq`);
-      eq_list[ei.report_id] = [];
+      app.eq_list[ei.report_id] = [];
     }
 
-    if(eq_list[ei.report_id].indexOf(ei.report_num) === -1) {
+    if(app.eq_list[ei.report_id].indexOf(ei.report_num) === -1) {
       console.log(`modules.eq[km] : new eq report`);
-      eq_list[ei.report_id].push(ei.report_num);
+      app.eq_list[ei.report_id].push(ei.report_num);
 
-      app.client.channels
-        .get(discord_config.notify_channel)
-        .send(
-          {
-            embed: {
-              title: `地震速報(高度利用) 第${ei.report_num}報`,
-              color: parseInt("0xff0000", 16),
-              fields: [
-                { name: '発生時刻',
-                  value: ei.origin_time,
-                  inline: true },
-                { name: '震央',
-                  value: ei.region_name,
-                  inline: true },
-                { name: '深さ',
-                  value: ei.depth,
-                  inline: true },
-                { name: '強さ(M)',
-                  value: `M${ei.magunitude}`,
-                  inline: true },
-                { name: '予想最大震度',
-                  value: `震度${ei.calcintensity}`,
-                  inline: true }
-              ]
-            }
-          }
-        );
+      const send_message = {
+        embed: {
+          title: `地震速報(高度利用) 第${ei.report_num}報`,
+          color: parseInt("0xff0000", 16),
+          fields: [
+            { name: '発生時刻',
+              value: ei.origin_time,
+              inline: true },
+            { name: '震央',
+              value: ei.region_name,
+              inline: true },
+            { name: '深さ',
+              value: ei.depth,
+              inline: true },
+            { name: '強さ(M)',
+              value: `M${ei.magunitude}`,
+              inline: true },
+            { name: '予想最大震度',
+              value: `震度${ei.calcintensity}`,
+              inline: true }
+          ]
+        }
+      }
+
+      const notify_channel = [discord_config.notify_channel];
+      if(
+        ei.calcintensity >= discord_config.emergency_notify_calcintensity &&
+        ei.report_num === '1'
+      ) notify_channel.push(discord_config.emergency_notify_channel);
+      
+      notify_channel.forEach(channel => {
+        discord.client.channels
+          .get(channel)
+          .send(send_message);
+      });
     }
   }
 }
@@ -59,11 +65,11 @@ exports.km = () => {
 exports.nhk = () => {
   nhkeq()
     .then(eq_data => {
-      if(nhkeq_list.indexOf(eq_data.$.Id) !== -1) {
+      if(app.nhkeq_list.indexOf(eq_data.$.Id) !== -1) {
         // console.log(`nhkeq : skipped duplicate data`);
         return;
       }
-      nhkeq_list.push(eq_data.$.Id);
+      app.nhkeq_list.push(eq_data.$.Id);
 
       const timestamp = new Date(eq_data.$.Time);
 
@@ -73,7 +79,7 @@ exports.nhk = () => {
       } // 起動時の時間より前の発表を無視する(#2)
 
       console.log(`modules.eq[nhk] : new eq`);
-      app.client.channels
+      discord.client.channels
         .get('487259165660545036')
         .send(
           {
