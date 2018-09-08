5571457a055fafeec51a40dff298f055ec06113b
Tomotada Saigusa <nirot1r@g-second.net>
2018-09-08 15:01:13 +0900

    #16 新強震モニタの画像を添付する

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 require('date-utils');
 const discord = require('./modules/discord');
-const generate_map = require('./lib/generate_map.js');
 
 let eq_list = { };
 let nhkeq_list = [];
@@ -12,5 +11,3 @@ exports.startup_time = (new Date()).toFormat('YYYYMMDDHH24MISS');
 console.log(`app[main] : welcome to ep8!`);
 
 discord.init();
-
-setTimeout(generate_map, 5000);
