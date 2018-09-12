9b970774b056eef324648273bb2bdd793e568f12
Tomotada Saigusa <nirot1r@g-second.net>
2018-09-13 04:25:29 +0900

    #67 不要なログのコメントアウト

--- src/modules/discord.js
+++ src/modules/discord.js
@@ -20,7 +20,7 @@ exports.sendMessage = message => {
 
   if(isLogin) {
 
-    console.log('[modules.discord] sendMessage > isLogin')
+    // console.log('[modules.discord] sendMessage > isLogin')
 
     discord.channels
       .get(discordConfig.notifyChannel)
@@ -28,7 +28,7 @@ exports.sendMessage = message => {
 
   } else {
 
-    console.log('[modules.discord] sendMessage > !isLogin')
+    console.error('[modules.discord] sendMessage > !isLogin')
     
   }
 
